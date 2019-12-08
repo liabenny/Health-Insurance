d77a17966cf1fac06da9dd200777ae45c3c84373
Jade Wang <wangyujue19960222@gmail.com>
2019-12-08 00:01:57 -0500

    build from str
    add categorized user input; add from string builder

--- application.py
+++ application.py
@@ -17,12 +17,14 @@ def list_to_dict(keys, user_input):
 def print_menu_list(header_list, user_input):
     print("************************Insurance Search List:************************")
     print(tabulate(user_input, headers=header_list, showindex="always"))
+    print()
 
 
 def get_insurance_type():
     values = [["Medical"], ["Dental"]]
     print(tabulate(values, headers=["Insurance Type"], showindex="always"))
     num = input("-Please enter the insurance type number: ")
+    print()
     return values[int(num)][0]
 
 
@@ -30,6 +32,7 @@ def get_subscriber():
     values = [["Individual"], ["Family"]]
     print(tabulate(values, headers=["Primary Subscriber"], showindex="always"))
     num = input("-Please enter the primary subscriber number: ")
+    print()
     return values[int(num)][0]
 
 
@@ -37,23 +40,33 @@ def get_cover_range():
     values = [["Adult and Child"], ["Child Only"], ["Adult Only"]]
     print(tabulate(values, headers=["Cover Range"], showindex="always"))
     num = input("-Please enter the cover range number: ")
+    print()
     return values[int(num)][0]
 
 
-def get_metal_level():
-    values = [['Platinum(Medical Only Applicable)'], ['Gold(Medical Only Applicable)'],
-              ['Silver(Medical Only Applicable)'], ['Bronze(Medical Only Applicable)'],
-              ['Catastrophic(Medical Only Applicable)'],
-              ['High(Dental Only Applicable)'],
-              ['Low(Dental Only Applicable)']]
+def get_metal_level_dental():
+    values = [['High'],
+              ['Low']]
     print(tabulate(values, headers=["Metal Level"], showindex="always"))
     num = input("-Please enter the metal level number: ")
+    print()
+    return values[int(num)][0]
+
+
+def get_metal_level_medical():
+    values = [['Platinum'], ['Gold'],
+              ['Silver'], ['Bronze'],
+              ['Catastrophic']]
+    print(tabulate(values, headers=["Metal Level"], showindex="always"))
+    num = input("-Please enter the metal level number: ")
+    print()
     return values[int(num)][0]
 
 
 def get_wellness():
     while True:
         num = input("-Whether need wellness program (YES/NO): ")
+        print()
         if num not in ("YES", "NO"):
             print("Oops Wrong input!")
             continue
@@ -65,6 +78,7 @@ def get_wellness():
 def get_out_of_country():
     while True:
         num = input("-Whether need out of country coverage (YES/NO): ")
+        print()
         if num not in ("YES", "NO"):
             print("Oops Wrong input!")
             continue
@@ -77,6 +91,7 @@ def get_exchange():
     values = [['On Exchange'], ['Off Exchange'], ['Both']]
     print(tabulate(values, headers=["Exchange Preference"], showindex="always"))
     num = input("-Please enter the exchange preference number: ")
+    print()
     return values[int(num)][0]
 
 
@@ -84,6 +99,7 @@ def get_payment():
     values = [["Copay"], ["Coin"]]
     print(tabulate(values, headers=["Payment Type"], showindex="always"))
     num = input("-Please enter the payment type number: ")
+    print()
     return values[int(num)][0]
 
 
@@ -94,6 +110,7 @@ def get_state():
                   'WI', 'WY']
     while True:
         state = input("-Please enter the state code: ")
+        print()
         if state not in state_list:
             print("Oops Wrong State Code!")
             continue
@@ -109,11 +126,13 @@ def get_family_type():
                   ['CoupleAndOneDependent'], ['CoupleAndTwoDependents'], ['CoupleAndThreeOrMoreDependents']]
     print(tabulate(value_list, headers=header_list, showindex="always"))
     type = input("-Please enter the family type number: ")
+    print()
     return value_list[int(type)][0]
 
 
 def get_age():
-    age = input("-Please enter the subscriber age: ")
+    age = input("-Please enter the primary subscriber age: ")
+    print()
     return age
 
 
@@ -121,19 +140,43 @@ def get_age():
 def input_processing():
     header_list = ["Insurance Type", "Subscriber", "Cover Range", "Metal Level", "Wellness Program",
                    "Exchange", "Out of Country", "Payment Type", "Age", "State", "Family Type"]
-    print_menu_list(header_list, [])
-    user_input = [[get_insurance_type(), get_subscriber(), get_cover_range(), get_metal_level(),
-                   get_wellness(),
-                   get_exchange(), get_out_of_country(), get_payment(), get_age(), get_state(),
-                   get_family_type()]]
+    user_input = []
     print_menu_list(header_list, user_input)
-    return list_to_dict(header_list, user_input[0])
+    type_str = get_insurance_type()
+    subscribe_str = get_subscriber()
+    if type_str == "Medical" and subscribe_str == 'Individual':
+        user_input = [type_str, subscribe_str, get_cover_range(), get_metal_level_medical(),
+                      get_wellness(),
+                      get_exchange(), get_out_of_country(), get_payment(), get_age(), get_state(),
+                      ""]
+
+    elif type_str == 'Dental' and subscribe_str == 'Individual':
+        user_input = [type_str, subscribe_str, get_cover_range(), get_metal_level_dental(),
+                      "",
+                      get_exchange(), get_out_of_country(), get_payment(), get_age(), get_state(),
+                      ""]
+
+    elif type_str == "Medical" and subscribe_str == 'Family':
+        user_input = [type_str, subscribe_str, get_cover_range(), get_metal_level_medical(),
+                      get_wellness(),
+                      get_exchange(), get_out_of_country(), get_payment(), get_age(), get_state(),
+                      get_family_type()]
+
+    elif type_str == 'Dental' and subscribe_str == 'Family':
+        user_input = [type_str, subscribe_str, get_cover_range(), get_metal_level_dental(),
+                      "",
+                      get_exchange(), get_out_of_country(), get_payment(), get_age(), get_state(),
+                      get_family_type()]
+
+    assert (len(header_list) == len(user_input))
+    print_menu_list(header_list, [user_input])
+    return list_to_dict(header_list, user_input)
 
 
 def main():
-    print("Welcome to Healthcare Insurance Database System!")
+    print("Welcome to Healthcare Insurance Database System!\n")
     while True:
-        print("======Initializing new search======")
+        print("======Initializing new search======\n")
         db.Query(input_processing()).run_query()
 
 
--- database.py
+++ database.py
@@ -1,12 +1,18 @@
 # database related code
 import tabulate as tabulate
 
+"""
+["Insurance Type", "Subscriber", "Cover Range", "Metal Level", "Wellness Program",
+"Exchange", "Out of Country", "Payment Type", "Age", "State", "Family Type"]
+"""
+
 
 class Query:
     """
     @:arg: request
     @:type: dictionary
     """
+
     def __init__(self, request):
         self.request = request
 
@@ -16,13 +22,46 @@ class Query:
         pass
 
     def build_str(self):
-        pass
+        return self.get_select() + self.get_from() + self.get_where()
+
 
     def get_select(self):
-        pass
+        res = "SELECT"
+        print(res)
+        return res
 
     def get_from(self):
-        pass
+        res = "FROM %s%s%s%s" % (
+            self.find_table_general(), self.find_table_insurance_type(self.request["Insurance Type"]),
+            self.find_table_subscriber(self.request["Subscriber"]),
+            self.find_table_payment(self.request["Payment Type"]))
+        print(res)
+        return res
 
     def get_where(self):
-        pass
+        res = "WHERE"
+        print(res)
+        return res
+
+    # The database tables can be apply for all query
+    def find_table_general(self):
+        return "plans, plan_benefit, plan_benefit_limitation, qhp_type"
+
+    def find_table_insurance_type(self, val):
+        if val == 'Medical':
+            return ', medical_plans, medical_metal_level_type'
+        else:  # Dental
+            return ', dental_plans, dental_metal_level_type'
+
+    def find_table_subscriber(self, val):
+        if val == 'Individual':
+            return ", rate_individual, age_type"
+        else:  # Family
+            return ", rate_family, family_type"
+
+    # TODO: TO BE DETERMINED
+    def find_table_payment(self, val):
+        if val == 'Copay':
+            return ", copay_type"
+        else:  # coin
+            return ", coin_type"
